5af549b5da3f6bb2a4d7c2e7f44c6183e40171ed
Yuhan Yao <yyao@caltech.edu>
2020-07-09 17:07:27 -0700

    update priority

--- scheduler/plan.py
+++ scheduler/plan.py
@@ -4,8 +4,11 @@
 Created on Sat Jul  4 08:35:28 2020
 
 @author: yuhanyao
+
+Eric's email: programpi = 'SRG'
 """
 import os
+import astropy.constants as const
 from copy import deepcopy
 import numpy as np
 from astropy.table import Table
@@ -25,7 +28,33 @@ ms = 5
 matplotlib.rcParams['lines.markersize']=ms
 
 
-def SRG_pointing(datestr = "20200704"):
+def get_theta_given_phi(a, b, c, phi):
+    """
+    ax + by + cz defines a plane
+    point coordinate (theta, phi)
+    """
+    a_prime = a * np.cos(phi)
+    b_prime = b * np.sin(phi)
+    theta_ = np.arctan(-1 * c / (a_prime + b_prime))
+    if theta_ < 0:
+        theta = theta_+np.pi
+    else:
+        theta = theta_
+    return theta
+
+
+def cos_in_sphere(theta1, phi1, theta2, phi2):
+    """
+    Here we use te cosine rule in Spherical trigonometry
+    https://en.wikipedia.org/wiki/Spherical_trigonometry
+    """
+    term1 = np.cos(theta2) * np.cos(theta1)
+    term2 = np.sin(theta2) * np.sin(theta1) * np.cos(phi1-phi2)
+    cos_rad = term1 + term2
+    return cos_rad
+
+
+def SRG_pointing(datestr = "20200709"):
     """
     Sun's movement during the year:
         http://community.dur.ac.uk/john.lucey/users/solar_year.html#:~:text=At%20the%20equinoxes%20at%20every,is%20on%20the%20celestial%20equator.&text=(September)%20equinox.-,At%20this%20time%20the%20Sun%20is%20at%20RA,h%2C%20Dec%20%3D%200.0%C2%B0.
@@ -49,18 +78,35 @@ def SRG_pointing(datestr = "20200704"):
     c = np.cos(sun_theta)
     # the plane that is perpendicular to the Sun is 
     # ax + by + cz = 0
-    phis = np.linspace(0, 2*np.pi, 360)
-    thetas = np.zeros(len(phis))
-    for i in range(len(phis)):
-        phi = phis[i]
-        a_prime = a * np.cos(phi)
-        b_prime = b * np.sin(phi)
-        theta_ = np.arctan(-1 * c / (a_prime + b_prime))
-        if theta_ < 0:
-            theta = theta_+np.pi
-        else:
-            theta = theta_
-        thetas[i] = theta
+    phis = [0]
+    thetas = [get_theta_given_phi(a, b, c, phis[0])]
+    sep_degs = []
+    for i in range(360):
+        phi_now = phis[-1]
+        theta_now = thetas[-1]
+        
+        # first of all, we assume that phi is uniformly scanned, but this is not the case
+        # so we use this as an initial guess, and adjust thhe step, such that
+        # the angle between consecutive points are the same (~1 degree, 360 points)
+        
+        step_deg = 1
+        step_rad = step_deg/180*np.pi
+        phi_trial = phi_now + step_rad
+        theta_trial = get_theta_given_phi(a, b, c, phi_trial)
+        cossep_trial = cos_in_sphere(theta_now, phi_now, theta_trial, phi_trial)
+        sep_deg_trial = np.arccos(cossep_trial)/np.pi*180
+        while abs(sep_deg_trial-1)>0.001:
+            step_deg /= sep_deg_trial
+            step_rad = step_deg/180*np.pi
+            phi_trial = phi_now + step_rad
+            theta_trial = get_theta_given_phi(a, b, c, phi_trial)
+            cossep_trial = cos_in_sphere(theta_now, phi_now, theta_trial, phi_trial)
+            sep_deg_trial = np.arccos(cossep_trial)/np.pi*180
+        phis.append(phi_trial)
+        thetas.append(theta_trial)
+        sep_degs.append(sep_deg_trial)
+    phis = np.array(phis)
+    thetas = np.array(thetas)
     alphas = phis
     decs = np.pi/2 - thetas
     return alphas/np.pi*180, decs/np.pi*180
@@ -124,6 +170,19 @@ def get_fieldid_srg(fields, alphas, decs):
     return flags
 
 
+def raDec2xy(ra,dec):
+    """
+    borrowed from Eric's ztf_sim notebook
+    """
+    # Using Aitoff projections (from Wiki) returns x-y coordinates on a plane of RA and Dec
+    theta = np.deg2rad(dec)
+    phi = np.deg2rad(ra)-np.pi #the range is [-pi,pi]
+    alpha = np.arccos(np.cos(theta)*np.cos(phi/2))
+    x = 2*np.cos(theta)*np.sin(phi/2)/np.sinc(alpha/np.pi) # The python's sinc is normalazid, hence the /pi
+    y = np.sin(theta)/np.sinc(alpha/np.pi)
+    return x, y
+
+
 def field_grids(datestr):
     dirstr = "bydate/"
     outdir = os.path.join(dirstr, datestr)
@@ -141,26 +200,18 @@ def field_grids(datestr):
     fields["filename"] = myfiles
     
     alphas, decs = SRG_pointing(datestr = datestr)
-    """
-    plt.plot(alphas, decs)
-    plt.plot(23.95, 2.33, 'ro')
-    plt.xlim(0, 50)
-    plt.ylim(0, 5)
-    """
-    
     flags = get_fieldid_srg(fields, alphas, decs)
     
-    ra_srg, dec_srg = get_projection_rad(alphas, decs)
-    ra_f, dec_f = get_projection_rad(fields["RA"], fields["Dec"])
-    
+    x_srg, y_srg = raDec2xy(alphas, decs)
     
     plt.figure(figsize=(18,10))
-    plt.subplot(111, projection="aitoff")
-    plt.plot(ra_srg, dec_srg, 'o', color = "gold", markersize = 6, alpha=1)
+    plt.subplot(111)
+    plt.plot(x_srg, y_srg, 'o', color = "gold", markersize = 6, alpha=1)
     
+    x_f, y_f = raDec2xy(fields["RA"], fields["Dec"])
     for i in range(len(fields)):
         rag, decg = np.loadtxt(fields["filename"][i]).T
-        ra_g, dec_g = get_projection_rad(rag, decg)
+        x_g, y_g = raDec2xy(rag, decg)
         if flags[i]>0:
             color = "r"
             zorder = 2
@@ -173,20 +224,23 @@ def field_grids(datestr):
             zorder = 1
             xoff = -2/180*np.pi
             yoff = -1/180*np.pi
-        plt.text(ra_f[i]+xoff, dec_f[i]+yoff, str(fields["ID"].data[i]), color = color, fontsize = fsnow)
+        plt.text(x_f[i]+xoff, y_f[i]+yoff, str(fields["ID"].data[i]), color = color, fontsize = fsnow)
         
-        ix1 = (rag>90)&(rag<180)
-        ix2 = (rag>180)&(rag<270)
+        ix1 = (rag>0)&(rag<90)
+        ix2 = (rag>270)&(rag<360)
         if np.sum(ix1)>=1 and np.sum(ix2)>=1:
-            plt.plot(ra_g[ix1], dec_g[ix1], '-', alpha=1, linewidth = 0.5, color = color,
+            plt.plot(x_g[ix1], y_g[ix1], '-', alpha=1, linewidth = 0.5, color = color,
                      zorder = zorder)
-            plt.plot(ra_g[ix2], dec_g[ix2], '-', alpha=1, linewidth = 0.5, color = color,
+            plt.plot(x_g[ix2], y_g[ix2], '-', alpha=1, linewidth = 0.5, color = color,
                      zorder = zorder)
         else:
-            plt.plot(ra_g, dec_g, '-', alpha=1, linewidth = 0.5, color = color,
+            plt.plot(x_g, y_g, '-', alpha=1, linewidth = 0.5, color = color,
                      zorder = zorder)
         
-    plt.title(datestr+": %d fields"%(np.sum(flags>0)))
+    titlename = datestr+": %d fields"%(np.sum(flags>0))
+    print (titlename)
+    plt.title(titlename)
+    plt.axis('off')
     plt.tight_layout()
     figfile = os.path.join(outdir, "fields_%s.png"%datestr)
     plt.savefig(figfile)
@@ -205,6 +259,11 @@ def field_grids(datestr):
     
     
 if __name__=="__main__":
-    datestr = "20200705"
-    field_grids(datestr)
+    for datestr in ["20200711", "20200712", "20200713", "20200714",
+                    "20200715", "20200716", "20200717", "20200718",
+                    "20200719", "20200720", "20200721", "20200722",
+                    "20200723", "20200724", "20200725", "20200726",
+                    "20200727", "20200728", "20200729", "20200730",
+                    "20200731", "20200801"]:
+        field_grids(datestr)
     
